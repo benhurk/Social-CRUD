c21f4abb5cbd7078eb911cddd8a9b8a7929cc197
benhur <benhurk2801@gmail.com>
2025-10-17 19:08:09 -0300

    feat: Setting up frontend dependencies

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api/",
+  withCredentials: false,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default axiosInstance;
